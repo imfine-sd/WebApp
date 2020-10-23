a8cf9f0086f06965d7b5dc7b4350db9374c9e21a
skim <suyoung.kim@im-fine.co.kr>
2020-10-23 12:49:19 +0900

    change docs

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/WebApp/precache-manifest.cf01b027efa3dd2e05a68849a97dff41.js"
+  "/WebApp/docs/precache-manifest.ca6dd5be0d2f21ac2a06d16e8d90cf28.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "web-app-demo"});
